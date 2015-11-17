639ee472ac15ce67f2ee7346bd41785878723066
Martin Zielinski <darthcoder1@gmail.com>
2015-11-17 21:08:52 +0100

    Added component to modify sprite animations

---
+++ Graphics/SpriteAnimationComponent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SMF
+{
+	public class SpriteAnimationComponent : MonoBehaviour
+	{
+		public Vector2 panSpeed;
+
+		private Vector2 currentOffset;
+		private Renderer renderComponent;
+
+
+		void Start()
+		{
+			renderComponent = GetComponent<Renderer>();
+			Debug.Assert(renderComponent);
+
+			renderComponent.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+			//for (int i = 0; i < renderComponent.sprite.uv.Length; ++i)
+				//renderComponent.sprite.rect
+		}
+
+		void Update()
+		{
+			Vector2 offset = panSpeed * Time.deltaTime;
+			currentOffset += offset;
+
+			renderComponent.material.mainTextureOffset = currentOffset;
+		}
+	}
+}
